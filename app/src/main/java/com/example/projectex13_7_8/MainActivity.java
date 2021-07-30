bba5f2e0b7d8f74a2f82bb4486acf0bbbb4fa4fc
최지우 <w2040@e-mirim.hs.kr>
2021-07-30 09:50:05 +0900

    Edit activity_main.xml commit

--- app/src/main/java/com/example/projectex13_7_8/MainActivity.java
+++ app/src/main/java/com/example/projectex13_7_8/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.projectex13_7_8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressBar progress;
+    SeekBar seek;
+    TextView textSeek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("ProgressBar & SeekBar");
+
+        progress = findViewById(R.id.progress1);
+        seek = findViewById(R.id.seeck1);
+        textSeek = findViewById(R.id.text_seek);
+        Button btninc = findViewById(R.id.btn_inc);
+        Button btndec = findViewById(R.id.btn_dec);
+
+        btninc.setOnClickListener(btnListener);
+        btndec.setOnClickListener(btnListener);
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                textSeek.setText("진행률:" + progress +"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btn_inc:
+                    progress.incrementProgressBy(10);
+                    break;
+                case R.id.btn_dec:
+                    progress.incrementProgressBy(-10);
+                    break;
+            }
+        }
+    };
 }
